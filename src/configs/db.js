626d739463fbe1f547227aafa6a696e869b0bb43
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-08-20 16:40:31 -0300

    add configs

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,16 +1,31 @@
 require('dotenv').config();
 
 module.exports = {
-  dialect: process.env.DIALECT,
-  host: process.env.HOST,
-  username: process.env.DB_USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  port: process.env.DB_PORT,
-  ssl: true,
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
+  development: {
+    username: 'XXXXXX',
+    password: 'XXXXXX',
+    database: 'party_playlist',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: process.env.DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: 'true',
+        rejectUnauthorized: 'false',
+      },
+    },
+    host: process.env.HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
   },
 };
