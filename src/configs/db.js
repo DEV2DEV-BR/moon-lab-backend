80b125d56c89efe1552c65781e4ac981703a96b5
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-08-20 16:50:56 -0300

    change configs in sequelize config

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,31 +1,23 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    username: 'XXXXXX',
-    password: 'XXXXXX',
-    database: 'party_playlist',
-    host: '127.0.0.1',
-    dialect: 'postgres',
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: process.env.DIALECT,
-    dialectOptions: {
-      ssl: {
-        require: 'true',
-        rejectUnauthorized: 'false',
-      },
-    },
-    host: process.env.HOST,
-    username: process.env.DB_USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.DB_PORT,
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
+  use_env_variable: 'DATABASE_URL',
+  dialect: process.env.DIALECT,
+  dialectOptions: {
+    ssl: {
+      require: 'true',
+      rejectUnauthorized: 'false',
     },
   },
+  host: process.env.HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DB_PORT,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+
 };
