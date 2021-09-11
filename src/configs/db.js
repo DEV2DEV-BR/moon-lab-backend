b2fdf4a07080566cf4db8ae1b9be5f8ebd697312
Daniel <danieldeandradelopes@gmail.com>
2021-09-11 17:36:10 -0300

    change config

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 
 module.exports = {
+  use_env_variable: process.env.DATABASE_URL,
   dialect: process.env.DIALECT,
   dialectOptions: {
-    ssl: true,
+    ssl: {
+      require: false,
+      rejectUnauthorized: false,
+    },
   },
   host: process.env.HOST,
   username: process.env.DB_USERNAME,
