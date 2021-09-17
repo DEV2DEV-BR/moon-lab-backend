875bad5563a4ff5d72b5e30ad389b4fa72280d03
Daniel <danieldeandradelopes@gmail.com>
2021-09-17 13:13:18 -0300

    add middleware

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const schemaValidator = require('../apps/middlewares/schemaValidator');
 
 const AuthenticationMiddleware = require('../apps/middlewares/authentication');
+const IsASuperUserMiddleware = require('../apps/middlewares/isASuperUser');
 
 const categorySchema = require('../schema/create.category.schema.json');
 const CategoriesController = require('../apps/controllers/CategoriesController');
@@ -10,9 +11,9 @@ const categories = new Router();
 
 categories.use(AuthenticationMiddleware);
 
-categories.post('/categories', schemaValidator(categorySchema), CategoriesController.create);
-categories.put('/categories/:id', CategoriesController.update);
-categories.delete('/categories/:id', CategoriesController.delete);
+categories.post('/categories', schemaValidator(categorySchema), IsASuperUserMiddleware, CategoriesController.create);
+categories.put('/categories/:id', IsASuperUserMiddleware, CategoriesController.update);
+categories.delete('/categories/:id', IsASuperUserMiddleware, CategoriesController.delete);
 categories.get('/categories/:id', CategoriesController.get);
 categories.get('/categories', CategoriesController.all);
 
