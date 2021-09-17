d5f36fef0699883c4dba988fff2c2d63b1a6eb83
Daniel <danieldeandradelopes@gmail.com>
2021-09-17 10:55:05 -0300

    add course

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -13,6 +13,7 @@ categories.use(AuthenticationMiddleware);
 categories.post('/categories', schemaValidator(categorySchema), CategoriesController.create);
 categories.put('/categories/:id', CategoriesController.update);
 categories.delete('/categories/:id', CategoriesController.delete);
-categories.get('/categories', CategoriesController.get);
+categories.get('/categories/:id', CategoriesController.get);
+categories.get('/categories', CategoriesController.all);
 
 module.exports = categories;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const categoriesRoutes = require('./categories.routes');
 const usersRoutes = require('./users.routes');
 const authenticationRoute = require('./authentication.routes');
+const coursesRoutes = require('./courses.routes');
 
 const routes = express();
 
-routes.use(categoriesRoutes);
-routes.use(usersRoutes);
 routes.use(authenticationRoute);
+routes.use(usersRoutes);
+routes.use(categoriesRoutes);
+routes.use(coursesRoutes);
 
 module.exports = routes;
